e29be90e3cadd0d7d23ec5229b8df9ed71b75437
Michal Koperski <mkoperpno@gmail.com>
2020-08-22 23:53:20 +0200

    next commit

--- src/main/java/com/example/todoapp/model/Task.java
+++ src/main/java/com/example/todoapp/model/Task.java
@@ -5,15 +5,17 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 @NoArgsConstructor
 @Table(name = "tasks")
-public class Task {
+class Task {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @NotBlank(message = "Task's description must not be empty")
     private String description;
     private boolean done;
 
