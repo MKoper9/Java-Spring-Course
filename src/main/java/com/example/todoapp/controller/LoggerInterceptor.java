141bedc364a8a43a02b21de74a0f36d8eb578d58
Michal Koperski <mkoperpno@gmail.com>
2020-09-14 19:53:09 +0200

    Create MvcInterceptor Class

--- src/main/java/com/example/todoapp/controller/LoggerInterceptor.java
+++ src/main/java/com/example/todoapp/controller/LoggerInterceptor.java
@@ -2,16 +2,19 @@ package com.example.todoapp.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@Component
 class LoggerInterceptor implements HandlerInterceptor {
     public static final Logger logger =LoggerFactory.getLogger(LoggerInterceptor.class);
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        logger.info("[doFilter] "+request.getMethod()+ " "+request.getRequestURI());
+        logger.info("[preHandle] "+request.getMethod()+ " "+request.getRequestURI());
         return true;
     }
 }
