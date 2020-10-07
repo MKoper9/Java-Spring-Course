0bedaf440a9e9e286d1a9018ddebd342ed6cfe0b
Michal Koperski <mkoperpno@gmail.com>
2020-10-07 20:57:00 +0200

    Add new Aspect

--- src/main/java/com/example/todoapp/controller/ProjectController.java
+++ src/main/java/com/example/todoapp/controller/ProjectController.java
@@ -4,6 +4,7 @@ import com.example.todoapp.logic.ProjectService;
 import com.example.todoapp.model.Project;
 import com.example.todoapp.model.ProjectStep;
 import com.example.todoapp.model.projection.ProjectWriteModel;
+import io.micrometer.core.annotation.Timed;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -50,6 +51,7 @@ public class ProjectController {
         return "projects";
     }
 
+    @Timed(value = "project.create.group",histogram = true, percentiles = {0.5, 0.95, 0.99})
     @PostMapping("/{id}")
     String createGroup(
             @ModelAttribute("project") ProjectWriteModel current,
